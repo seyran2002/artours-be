03af72df4db6656903802d5b50c9a911a4f95981
seyran2002 <seyranavetisyan2002@gmail.com>
2026-08-16 17:42:53 +0400

    fix corse errors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'https://artours.am',
+      'https://www.artours.am',
+      'http://localhost:5173',
+    ],
     credentials: true,
   })
 
