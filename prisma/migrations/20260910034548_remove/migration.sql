fc55f49532c9f2d91b2755ed83fd4fe3993398b0
seyran2002 <seyranavetisyan2002@gmail.com>
2026-09-13 13:20:17 +0400

    migration changed

--- prisma/migrations/20260910034548_remove/migration.sql
+++ prisma/migrations/20260910034548_remove/migration.sql
@@ -1,14 +1,27 @@
 -- AlterEnum
+
 ALTER TYPE "BookingType" ADD VALUE 'LOCATION';
 
 -- DropForeignKey
-ALTER TABLE "_TagToTransfer" DROP CONSTRAINT "_TagToTransfer_A_fkey";
+
+ALTER TABLE "_TagToTransfer"
+DROP CONSTRAINT IF EXISTS "_TagToTransfer_A_fkey";
 
 -- DropForeignKey
-ALTER TABLE "_TagToTransfer" DROP CONSTRAINT "_TagToTransfer_B_fkey";
+
+ALTER TABLE "_TagToTransfer"
+DROP CONSTRAINT IF EXISTS "_TagToTransfer_B_fkey";
 
 -- AddForeignKey
-ALTER TABLE "_TagToTransfer" ADD CONSTRAINT "_TagToTransfer_A_fkey" FOREIGN KEY ("A") REFERENCES "Transfer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "_TagToTransfer"
+ADD CONSTRAINT "_TagToTransfer_A_fkey"
+FOREIGN KEY ("A") REFERENCES "Transfer"("id")
+ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_TagToTransfer" ADD CONSTRAINT "_TagToTransfer_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "_TagToTransfer"
+ADD CONSTRAINT "_TagToTransfer_B_fkey"
+FOREIGN KEY ("B") REFERENCES "Tag"("id")
+ON DELETE CASCADE ON UPDATE CASCADE;
